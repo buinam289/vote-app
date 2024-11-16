43474953fb2c5cac4f7bdedc27fa65de77f08675
rogerbui@nvg.vn <rogerbui@nvg.vn>
2024-11-16 14:45:36 +0700

    extract uploadImage component

--- src/components/UpdateProfile.tsx
+++ src/components/UpdateProfile.tsx
@@ -4,52 +4,15 @@ import { useState, useEffect } from "react";
 import { updateProfile } from "@/app/actions/profileUpdate";
 import { getProfile, Profile } from "@/app/actions/profileGet";
 import { uploadProfileImage } from "@/app/actions/profileUploadImage";
-import Image from "next/image";
+import UploadImage from "./UploadImage";
 
 export default function UpdateProfile() {
     const [profile, setProfile] = useState<Profile | null>(null);
+    const [profileImagePath, setProfileImagePath] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
     const [formSaving, setFormLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
     const [showCheckmark, setShowCheckmark] = useState(false);
-
-
-    const [file, setFile] = useState<File | null>(null);
-    const [uploading, setUploading] = useState(false);
-    const [filePath, setFilePath] = useState<string | null>(null);
-
-    // Handle file input change
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const selectedFile = event.target.files ? event.target.files[0] : null;
-        setFile(selectedFile);
-    };
-
-    // Handle form submission
-    const handleSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-
-        if (!file) {
-            alert("Please select a file to upload.");
-            return;
-        }
-
-        setUploading(true);
-
-        const formData = new FormData();
-        formData.append("file", file);
-
-        try {
-            // Call the server action to handle the file upload
-            const uploadedFilePath = await uploadProfileImage(formData);
-
-            // Update the file path state to display the uploaded image
-            setFilePath(uploadedFilePath);
-        } catch (error) {
-            alert("Error uploading the file.");
-        } finally {
-            setUploading(false);
-        }
-    };
 
     useEffect(() => {
         async function fetchProfile() {
@@ -59,7 +22,7 @@ export default function UpdateProfile() {
                     setError("Unauthorized");
                     return;
                 }
-                setFilePath(data.image);
+                setProfileImagePath(data.image);
                 setProfile(data);
             } catch (err) {
                 console.error(err);
@@ -93,29 +56,7 @@ export default function UpdateProfile() {
         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
             <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
                 <h1 className="text-2xl font-bold mb-6">Edit Profile</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-6">
-                        <label htmlFor="image" className="block text-sm font-medium text-gray-700">
-                            Profile Image:
-                        </label>
-                        <input onChange={handleFileChange}
-                            type="file"
-                            id="image"
-                            name="image"
-                            accept="image/*"
-                            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                        />
-                        {filePath && <Image src={filePath} alt="Profile" width={100} height={100} className="mt-2" />}
-                    </div>
-
-                    <button
-                        type="submit"
-                        disabled={uploading}
-                        className="px-4 py-2 bg-blue-600 text-white rounded disabled:bg-gray-400"
-                    >
-                        {uploading ? "Uploading..." : "Upload"}
-                    </button>
-                </form>
+                <UploadImage title="Upload your profile image" filePath={profileImagePath} uploadImage={uploadProfileImage} />
                 <br />
                 <form onSubmit={handleUpdateProfile}>
                     <div className="mb-6">
