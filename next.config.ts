8ad2874177a4a4f7ca64d21e0c4c5af323ad88b3
rogerbui@nvg.vn <rogerbui@nvg.vn>
2024-11-17 11:20:21 +0700

    add Google and Facebook authentication providers and update image domains

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['avatars.githubusercontent.com'], // Add GitHub's image domain here
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'], // Add GitHub's image domain here
   },
 };
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
+import Facebook from "next-auth/providers/facebook";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/utils";
@@ -8,7 +10,9 @@ import { prisma } from "@/lib/utils";
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
-        GitHub,
+        Facebook,
+        Google,
+        GitHub
     ],
     basePath: "/auth",
 })
